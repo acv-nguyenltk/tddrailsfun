edb0476239840d14b527c5cbfa2385cb04d66fc6
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-11 13:54:41 +0700

    Changing modifier price in Products and another way do that

---
+++ db/migrate/20180811064445_change_products_price.rb
@@ -0,0 +1,18 @@
+class ChangeProductsPrice < ActiveRecord::Migration[5.2]
+  # def up
+  #   change_column :products, :price, :decimal, scale: 2, precision: 10
+  # end
+  #
+  # def down
+  #   change_column :products, :price, :decimal, scale: 2, precision: 6
+  # end
+
+  def change
+    reversible do |dir|
+      change_table :products do |t|
+        dir.up   { t.change :price, :decimal, scale: 2, precision: 10 }
+        dir.down { t.change :price, :decimal, scale: 2, precision: 6 }
+      end
+    end
+  end
+end
