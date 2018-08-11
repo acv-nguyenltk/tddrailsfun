c1389a437f45e8738ed1977aeb0223003968f2fc
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-11 12:54:21 +0700

    migration table products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,8 +2,10 @@ require 'rails-helper'
 
 RSpec.describe Product, type: :model do
   before(:each) do
-    @product = Product.create!(title: 'aaa', description: 'description', price: 12) 
+    @product = Product.create!(title: 'aaa', description: 'description', price: 12)
   end
+
+
 
 
 end
