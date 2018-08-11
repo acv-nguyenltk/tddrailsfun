75ad3d8e7059e4877dfbad2f8652bc6e6cc2debf
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-11 12:46:41 +0700

    create product_spec.rb

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,9 @@
+require 'rails-helper'
+
+RSpec.describe Product, type: :model do
+  before(:each) do
+    @product = Product.create!(title: 'aaa', description: 'description', price: 12) 
+  end
+
+
+end
