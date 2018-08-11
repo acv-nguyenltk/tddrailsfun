dd754ca7f4fd71d6b9f3ec30153eb021ce4449a0
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-11 13:42:57 +0700

    add another way reference 2 tables, mirgrate

--- db/migrate/20180811063632_add_reference_to_categories_in_products.rb
+++ db/migrate/20180811063632_add_reference_to_categories_in_products.rb
@@ -1,4 +1,6 @@
 class AddReferenceToCategoriesInProducts < ActiveRecord::Migration[5.2]
   def change
+    add_reference :products, :category, index: true
+    # add_reference :products, :category, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_11_063006) do
+ActiveRecord::Schema.define(version: 2018_08_11_063632) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2018_08_11_063006) do
     t.datetime "updated_at", null: false
     t.decimal "price", precision: 6, scale: 2
     t.boolean "published"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
