dc8f50d53489318c52d2ef0c5aef321623a6c3a5
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-11 14:42:47 +0700

    add validate lowercase_title and rspec it

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ApplicationRecord
   validates :title, :description, :price, presence: true
+  validates :price, numericality: true
+  validates :price, numericality: { greater_than: 0 }
+  before_save :set_title_lowercase
+
+  def set_title_lowercase
+    self.title = title.downcase
+  end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,19 +3,26 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   fields = ['title', 'description', 'price']
   before(:each) do
-    @product = Product.create!(title: 'aaa', description: 'description', price: 12)
+    @product = Product.create!(title: 'AAA', description: 'description', price: 1)
   end
 
-  describe 'Validations'do
+  describe 'Validations' do
     fields.each do |field|
       it { should validate_presence_of(field) }
     end
+    it 'lowercase' do
+      expect(@product.title).to eq 'aaa'
+    end
+
   end
 
-  # describe 'Numerically' do
-  #   it { should validate_numericality_of(:price) }
-  #   it { should validate_numericality_of(:price).greater_than_to(0) }
-  # end
+  describe 'Association' do
+  end
+
+  describe 'Numerically' do
+    it { should validate_numericality_of(:price) }
+    it { should validate_numericality_of(:price).is_greater_than(0) }
+  end
 
 
 end
