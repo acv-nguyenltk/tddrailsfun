5fbcf12694a4db5fb2cce18e2f1b0e3b539d7d9e
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-12 18:23:17 +0700

    add create controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,17 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new
   end
+
+  def create
+    product_params = params.require(:product).permit( :title,
+      :description, :level, :category_id, :price, :country, :published)
+      @product = Product.new(product_params)
+      if @product.save
+        flash[:notice] = "Created"
+      redirect_to products_url
+      else
+        flash[:notice] = "Not yet"
+      render :new
+      end
+  end
 end
